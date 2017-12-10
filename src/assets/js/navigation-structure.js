c9442746b7589fb6752b425a0efab08a7cd26c0e
Josue Plaza <josue@parlevelsystems.com>
2017-12-09 21:34:43 -0600

    convert css to sass, add img and js asset folders

---
+++ src/assets/js/navigation-structure.js
@@ -0,0 +1,330 @@
+var navigation = [
+    {
+        id: 1,
+        title: "Category 1",
+        areas: [
+        {
+            id: 1,
+            title: "Area 1",
+            sub_areas: []
+        },
+        {
+            id: 2,
+            title: "Area 2",
+            sub_areas: []
+        },
+        {
+            id: 3,
+            title: "Area 3",
+            sub_areas: []
+        },
+        {
+            id: 4,
+            title: "Area 4",
+            sub_areas: []
+        },
+        {
+            id: 5,
+            title: "Area 5",
+            sub_areas: []
+        }
+        ]
+    },
+    {
+        id: 2,
+        title: "Category 2",
+        areas: [
+            {
+                id: 1,
+                title: "Area 1",
+                sub_areas: [
+                    {
+                        id: 1,
+                        title: "Sub-Area 1",
+                        children: [],
+                    },
+                    {
+                        id: 2,
+                        title: "Sub-Area 2",
+                        children: [],
+                    },
+                    {
+                        id: 3,
+                        title: "Sub-Area 3",
+                        children: [],
+                    }
+                ]
+            },
+            {
+                id: 2,
+                title: "Area 2",
+                sub_areas: []
+            },
+            {
+                id: 3,
+                title: "Area 3",
+                sub_areas: []
+            },
+            {
+                id: 4,
+                title: "Area 4",
+                sub_areas: []
+            },
+            {
+                id: 5,
+                title: "Area 5",
+                sub_areas: []
+            },
+            {
+                id: 6,
+                title: "Area 6",
+                sub_areas: []
+            },
+            {
+                id: 7,
+                title: "Area 7",
+                sub_areas: []
+            },
+            {
+                id: 8,
+                title: "Area 8",
+                sub_areas: []
+            },
+            {
+                id: 9,
+                title: "Area 9",
+                sub_areas: []
+            },
+            {
+                id: 10,
+                title: "Area 10",
+                sub_areas: []
+            },
+            {
+                id: 11,
+                title: "Area 11",
+                sub_areas: []
+            },
+            {
+                id: 12,
+                title: "Area 12",
+                sub_areas: []
+            },
+            {
+                id: 13,
+                title: "Area 13",
+                sub_areas: []
+            },
+            {
+                id: 14,
+                title: "Area 14",
+                sub_areas: []
+            },
+            {
+                id: 15,
+                title: "Area 15",
+                sub_areas: []
+            },
+            {
+                id: 16,
+                title: "Area 16",
+                sub_areas: []
+            },
+            {
+                id: 17,
+                title: "Area 17",
+                sub_areas: []
+            },
+            {
+                id: 18,
+                title: "Area 18",
+                sub_areas: []
+            },
+            {
+                id: 19,
+                title: "Area 19",
+                sub_areas: []
+            },
+            {
+                id: 20,
+                title: "Area 20",
+                sub_areas: []
+            },
+            {
+                id: 21,
+                title: "Area 21",
+                sub_areas: []
+            },
+            {
+                id: 22,
+                title: "Area 22",
+                sub_areas: []
+            },
+            {
+                id: 23,
+                title: "Area 23",
+                sub_areas: []
+            },
+            {
+                id: 24,
+                title: "Area 24",
+                sub_areas: []
+            },
+            {
+                id: 25,
+                title: "Area 25",
+                sub_areas: []
+            },
+            {
+                id: 26,
+                title: "Area 26",
+                sub_areas: []
+            },
+            {
+                id: 27,
+                title: "Area 27",
+                sub_areas: []
+            },
+            {
+                id: 28,
+                title: "Area 28",
+                sub_areas: []
+            },
+            {
+                id: 29,
+                title: "Area 29",
+                sub_areas: []
+            },
+            {
+                id: 30,
+                title: "Area 30",
+                sub_areas: []
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Category 3",
+        areas: [
+        {
+            id: 1,
+            title: "Area 1",
+            sub_areas: [
+            {
+                id: 1,
+                title: "Sub-Area 1",
+                children: [
+                {
+                    id: 1,
+                    title: "Child-Area 1",
+                },
+                {
+                    id: 2,
+                    title: "Child-Area 2",
+                },
+                {
+                    id: 3,
+                    title: "Child-Area 3",
+                },
+                {
+                    id: 4,
+                    title: "Child-Area 4",
+                },
+                {
+                    id: 5,
+                    title: "Child-Area 5",
+                },
+                {
+                    id: 6,
+                    title: "Child-Area 6",
+                },
+
+                ],
+            },
+            {
+                id: 2,
+                title: "Sub-Area 2",
+                children: [],
+            },
+            ]
+        },
+        {
+            id: 2,
+            title: "Area 2",
+            sub_areas: []
+        },
+        {
+            id: 2,
+            title: "Area 3",
+            sub_areas: []
+        },
+        {
+            id: 3,
+            title: "Area 4",
+            sub_areas: []
+        }
+        ]
+    },
+    {
+        id: 4,
+        title: "Category 4",
+        areas: [
+        {
+            id: 1,
+            title: "Area 1",
+            sub_areas: [
+            {
+                id: 1,
+                title: "Sub-Area 1",
+                children: [],
+            }
+            ]
+        },
+        {
+            id: 1,
+            title: "Area 2",
+            sub_areas: [
+            {
+                id: 1,
+                title: "Sub-Area 1",
+                children: [],
+            }
+            ]
+        },
+        {
+            id: 3,
+            title: "Area 3",
+            sub_areas: [
+            {
+                id: 1,
+                title: "Sub-Area 1",
+                children: [],
+            }
+            ]
+        },
+        {
+            id: 4,
+            title: "Area 4",
+            sub_areas: [
+            {
+                id: 1,
+                title: "Sub-Area 1",
+                children: [],
+            }
+            ]
+        },
+        {
+            id: 5,
+            title: "Area 5",
+            sub_areas: [
+            {
+                id: 1,
+                title: "Sub-Area 1",
+                children: [],
+            }
+            ]
+        }
+        ]
+    }
+];
+module.exports = navigation;
