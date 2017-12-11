aae31fb6f60959f324767060bdf08e984a36386a
Josue Plaza <josue@parlevelsystems.com>
2017-12-10 18:13:54 -0600

    routing setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component';
+import { AreaComponent } from './area/area.component';
+import { SubAreaComponent } from './subArea/subArea.component';
+import { ChildComponent } from './child/child.component';
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: ':categoryId',
+        component: CategoryComponent
+    },
+    {
+        path: ':categoryId/:areaId',
+        component: AreaComponent
+    },
+    {
+        path: ':categoryId/:areaId/:subAreaId',
+        component: SubAreaComponent
+    },
+    {
+        path: ':categoryId/:areaId/:subAreaId/:childId',
+        component: ChildComponent
+    }
+];
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        CategoryComponent,
+        AreaComponent,
+        SubAreaComponent,
+        ChildComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    // entryComponents: [
+    //     HomeComponent,
+    //     CategoryComponent,
+    //     AreaComponent,
+    //     SubAreaComponent,
+    //     ChildComponent
+    // ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/subArea/subArea.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-subArea',
+    templateUrl: './subArea.component.html'
+})
+export class SubAreaComponent {
+	
+}
